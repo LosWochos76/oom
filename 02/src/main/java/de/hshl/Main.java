fb4fc808bbcac833f53af7ac141c4edcb1cb410c
Alex <alexander.stuckenholz@gmail.com>
2022-01-05 15:15:20 +0100

    Working on chapter 03

--- 02/src/main/java/de/hshl/Main.java
+++ 02/src/main/java/de/hshl/Main.java
@@ -1,45 +1,6 @@
 package de.hshl;
 
-import java.util.ArrayList;
-
 public class Main {
-    public static Bruch[] erzeugeBruchArray() {
-        Bruch[] brueche = new Bruch[10];
-        for (int i=0; i<10; i++) {
-            brueche[i] = new Bruch(1, i+1);
-        }
-
-        return brueche;
-    }
-
-    public static double bildeSumme(Bruch[] brueche) {
-        double summe = 0;
-        for (int i=0; i<10; i++) {
-            summe += brueche[i].getDezimalzahl();
-        }
-
-        return summe;
-    }
-
-    public static ArrayList erzeugeBruchArrayList() {
-        ArrayList brueche = new ArrayList();
-        for (int i=0; i<10; i++) {
-            brueche.add(new Bruch(1, i+1));
-        }
-
-        return brueche;
-    }
-
-    public static double bildeSumme(ArrayList brueche) {
-        double summe = 0;
-        for (int i=0; i<brueche.size(); i++) {
-            Bruch b = (Bruch)brueche.get(i);
-            summe += b.getDezimalzahl();
-        }
-
-        return summe;
-    }
-
     public static void main(String[] args) {
         Bruch b = new Bruch(1,3);
         Bruch c = new Bruch(1,4);
@@ -47,16 +8,5 @@ public class Main {
         c.ausgeben();
         b.multipliziere(c).ausgeben();
         System.out.println(b.equals(c));
-
-        // Wirft eine Ausnahme:
-        // Bruch d = new Bruch(1,0);
-
-        Bruch[] brueche_array = erzeugeBruchArray();
-        double summe1 = bildeSumme(brueche_array);
-        System.out.println("Summe aus dem Array=" + summe1);
-
-        ArrayList brueche_arraylist = erzeugeBruchArrayList();
-        double summe2 = bildeSumme(brueche_arraylist);
-        System.out.println("Summe aus der ArrayList=" + summe2);
     }
 }
---
+++ 03/src/main/java/de/hshl/Main.java
@@ -0,0 +1,52 @@
+package de.hshl;
+
+import java.util.ArrayList;
+
+public class Main {
+    public static Bruch[] erzeugeBruchArray() {
+        Bruch[] brueche = new Bruch[10];
+        for (int i=0; i<10; i++) {
+            brueche[i] = new Bruch(1, i+1);
+        }
+
+        return brueche;
+    }
+
+    public static double bildeSumme(Bruch[] brueche) {
+        double summe = 0;
+        for (int i=0; i<10; i++) {
+            summe += brueche[i].getDezimalzahl();
+        }
+
+        return summe;
+    }
+
+    public static ArrayList erzeugeBruchArrayList() {
+        ArrayList brueche = new ArrayList();
+        for (int i=0; i<10; i++) {
+            brueche.add(new Bruch(1, i+1));
+        }
+
+        return brueche;
+    }
+
+    public static double bildeSumme(ArrayList brueche) {
+        double summe = 0;
+        for (int i=0; i<brueche.size(); i++) {
+            Bruch b = (Bruch)brueche.get(i);
+            summe += b.getDezimalzahl();
+        }
+
+        return summe;
+    }
+
+    public static void main(String[] args) {
+        Bruch[] brueche_array = erzeugeBruchArray();
+        double summe1 = bildeSumme(brueche_array);
+        System.out.println("Summe aus dem Array=" + summe1);
+
+        ArrayList brueche_arraylist = erzeugeBruchArrayList();
+        double summe2 = bildeSumme(brueche_arraylist);
+        System.out.println("Summe aus der ArrayList=" + summe2);
+    }
+}
